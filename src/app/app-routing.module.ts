0e3d306b4d5357525f2a701d85dae4667b75a205
danish1594 <aroradanish533@gmail.com>
2020-11-24 00:01:57 +0530

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ImagesComponent } from './images/images.component';
+import { LoginComponent } from './login/login.component';
+import { VideosComponent } from './videos/videos.component';
 
-const routes: Routes = [];
+const routes: Routes =[{path:'',component:HomeComponent},{path:'images',component:ImagesComponent},
+{path:'videos',component:VideosComponent},{path:'login',component:LoginComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ImagesComponent } from './images/images.component';
+import { VideosComponent } from './videos/videos.component';
+
+import { LoginComponent } from './login/login.component';
+import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ImagesComponent,
+    VideosComponent,
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
